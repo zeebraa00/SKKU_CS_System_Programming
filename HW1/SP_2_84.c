2781eaeee05791dcad6246503d5d30cc0053d8a5
zeebraa00 <zeebraa00@gmail.com>
2020-10-11 20:36:45 +0900

    [add] SP_2_84.c

---
+++ HW1/SP_2_84.c
@@ -0,0 +1,12 @@
+/*
+
+A. The number 5.0
+=> E = 2, M = 1.25, f = 0.25, V = 101
+
+B. The largest odd integer that can be represented exactly
+=> E = n, M = 1.1...1, f = 1...1, V = 2^E * M (2^(k-1) > n)
+
+C. The reciprocal of the smallest positive normalized value
+=> E = -2^(k-1) + 2, M = 1, f = 0, V = 2^(2-2^(k-1))
+
+*/
