23379b07086ebc61337d347fa03220194b230fce
zeebraa00 <zeebraa00@gmail.com>
2020-11-09 23:44:00 +0900

    [edit] directory

---
+++ Exercise/SP_2_83.c
@@ -0,0 +1,9 @@
+int float_ge(float x, float y) {
+    unsigned ux = f2u(x);
+    unsigned uy = f2u(y);
+    // Get the sign bits
+    unsigned sx = ux >> 31;
+    unsigned sy = uy >> 31;
+
+    return ((!sx&&sy)||(!sx&&!sy&&ux>=uy)||(sx&&sy&&ux<=uy)||(ux<<1==0&&uy<<1==0));
+}
